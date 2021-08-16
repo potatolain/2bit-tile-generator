b47ccfc354390370f9033d8c25277356c44eef7e
cppchriscpp <admin@cpprograms.net>
2021-08-15 22:26:41 -0400

    Swap gb colors to support gbtd

--- src/constants/palette-constants.js
+++ src/constants/palette-constants.js
@@ -7,7 +7,9 @@ export const availablePalettes = [
 ];
 
 export const paletteData = {
-    'Gameboy': [0x000000ff, 0x444444ff, 0xaaaaaaff, 0xffffffff],
+    // NOTE: These colors are exact matches that gbtd understands. Don't change just to make pretty!
+    // (or at least make a separate palette)
+    'Gameboy': [0x000000ff, 0x808080ff, 0xc0c0c0ff, 0xffffffff],
     'NES Default Blue': [0x000000ff, 0x00008fff, 0x4488ffff, 0x99ccffff],
     'NES Default Red': [0x000000ff, 0x550000ff, 0x993300ff, 0xdd6644ff],
     'NES Default Green': [0x000000ff, 0x113300ff, 0x336600ff, 0x77bb00ff],
