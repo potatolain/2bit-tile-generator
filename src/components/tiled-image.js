6f64e0970ff814299830320a7f19dcbedbfa25e2
cppchriscpp <admin@cpprograms.net>
2021-09-05 02:31:20 -0400

    Remove old unused code

--- src/components/tiled-image.js
+++ src/components/tiled-image.js
@@ -9,11 +9,6 @@ import { TILE_PREVIEW_IDS } from '../constants/tile-constants';
 // - src: The image src to use. (can be base64, or whatever)
 // - size: The number of images to tile together. A value of 3 would render a 3x3 grid.
 export default class TiledImage extends React.Component {
-  renderX() {
-    return <div className="tile-preview-collection">
-    {[...Array(this.props.size * this.props.size).keys()].map(a => <img alt={"tile" + a} src={this.props.src} key={"preview-" + a}></img>)}
-  </div>;
-  }
   render() {
     const allSrc = TILE_PREVIEW_IDS[this.props.tileId].map(id => this.props.tileImages[id]);
     return <div className="tile-preview-collection">
